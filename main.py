2bbfae16058313fedb10e8bbef07151ead4e5276
Anthony <anthony@debian.Anton>
2024-11-14 06:50:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+#from pygame.locals import *
+import sys
+
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+FPS = 30
+
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    window.fill('red')
+    pygame.display.update()
+    clock.tick(FPS)
