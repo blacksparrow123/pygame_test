3cf09cbf62b8163fce262aa5aae503ffa951e221
blacksparrow123 <ahriman2@mail.rucd>
2024-11-17 09:23:31 +0300

    ex 5.5

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ FPS = 30
 BALL_H_W = 100
 MAX_WIDTH = WINDOW_WIDTH - BALL_H_W
 MAX_HEIGHT = WINDOW_HEIGHT - BALL_H_W
+TARGET_X = 400
+TARGET_Y = 320
+TARGET_WIDTH_HEIGHT = 120
+N_PIXELS_TO_MOVE = 3
 
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 ballImage = pygame.image.load("assets/images/ball.png")
+targetImage = pygame.image.load('assets/images/target.jpg')
 ballX = random.randrange(MAX_WIDTH)
 ballY = random.randrange(MAX_HEIGHT)
-ballRect = pygame.Rect(ballX, ballY, BALL_H_W, BALL_H_W)
+targetRect = pygame.Rect(TARGET_X, TARGET_Y, TARGET_WIDTH_HEIGHT, TARGET_WIDTH_HEIGHT)
+
 
 while True:
     for event in pygame.event.get():
@@ -25,12 +31,21 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            if ballRect.collidepoint(event.pos):
-                ballX = random.randrange(MAX_WIDTH)
-                ballY = random.randrange(MAX_HEIGHT)
-                ballRect = pygame.Rect(ballX, ballY, BALL_H_W, BALL_H_W)
+    keyPressedTuple = pygame.key.get_pressed()
+    if keyPressedTuple[pygame.K_LEFT]:
+        ballX = ballX - N_PIXELS_TO_MOVE
+    if keyPressedTuple[pygame.K_RIGHT]:
+        ballX = ballX + N_PIXELS_TO_MOVE
+    if keyPressedTuple[pygame.K_UP]:
+        ballY = ballY - N_PIXELS_TO_MOVE
+    if keyPressedTuple[pygame.K_DOWN]:
+        ballY = ballY + N_PIXELS_TO_MOVE
+    print(keyPressedTuple)
+    ballRect = pygame.Rect(ballX, ballY, BALL_H_W, BALL_H_W)
+    if ballRect.colliderect(targetRect):
+        print('Ball is touching the target')
     window.fill("red")
     window.blit(ballImage, (ballX, ballY))
+    window.blit(targetImage, (TARGET_X, TARGET_Y))
     pygame.display.update()
     clock.tick(FPS)
